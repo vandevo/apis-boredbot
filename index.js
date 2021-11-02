85fbd93ff42aab420b719e5a5f244c0fd82df9f0
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-11-02 15:22:06 +0000

    master

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-/**
-Challenge: 
+document.getElementById('getActivity').addEventListener("click", function(){
 
-- Add some styling! Be creative, play with layout a bit
-  (Spoiler: I'm going to use a single-column flexbox layout)
-  add some color, grab a font from Google fonts - whatever
-  you'd like!
-*/
 
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
+  fetch('https://apis.scrimba.com/bored/api/activity')
+  .then(response => response.json())
+  .then(data => {
+    
+    document.getElementById('activity').textContent = data.activity
+    
+  });
+
+})
+
+
